c35c385e201d302127b935b5ac2a7828cad186c4
Guillaume Sparrow-Pepin <gsparrowpepin@putneyschool.org>
2018-02-21 13:50:18 -0500

    Initial Commit

---
+++ TEST/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TEST
+//
+//  Created by Guillaume Sparrow-Pepin on 2/21/18.
+//  Copyright © 2018 Guillaume Sparrow-Pepin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
